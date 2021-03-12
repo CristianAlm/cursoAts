13e75330ee34d15a822c3c2509dda35416abdc74
Cristian Almiron <cristianalmiron1988@gmail.com>
2021-03-12 05:30:42 -0300

    Ultimo ejercicio de la seccion operadores, ahora toca los ejercicios de condicionales

--- ats-java/src/operador/raizEcuacionSegundoGrado.java
+++ ats-java/src/operador/raizEcuacionSegundoGrado.java
@@ -1,5 +1,8 @@
 package operador;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /*
  * Construir un programa que calcule y muestre por pantalla las raices de la ecuacion de segundo grado
  * de coeficientes reales.
@@ -12,7 +15,42 @@ public class raizEcuacionSegundoGrado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		double a,b,c,resultado1 = 0,resultado2 = 0;
+		
+		a = pedirValores();
+		b = pedirValores();
+		c = pedirValores();
+		
+		programa(a,b,c, resultado1,resultado2);
 
+	}
+
+	private static void programa(double a, double b, double c, double resultado1, double resultado2) {
+		// TODO Auto-generated method stub
+		
+		resultado1 = (-b+Math.sqrt(Math.pow(b, 2) - (4*a*c))) / (2*a);
+		resultado2 = (-b-Math.sqrt(Math.pow(b, 2) - (4*a*c))) / (2*a);
+		
+		System.out.println("La Raiz1 es: " + resultado1 + " y la Raiz2 es: " + resultado2);
+		
+	}
+
+	private static int pedirValores() {
+		// TODO Auto-generated method stub
+		int valorLocal = 0;
+		
+		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			System.out.println("Ingrese el valor: ");
+			valorLocal = Integer.valueOf(entrada.readLine());
+		}	
+		catch (Exception exc) {
+			// TODO: handle exception
+			System.out.println(exc);
+		}
+		return valorLocal;
 	}
 
 }
